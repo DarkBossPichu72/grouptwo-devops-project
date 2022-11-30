9bcf33fecec80645c3f511f7fbfddb695caf1bac
abidinza1 <abidinz2102@gmail.com>
2022-11-30 15:58:54 +0800

    i have done the review page

--- src/main/java/com/dvops/maven/eclipse/HomeSevlet.java
+++ src/main/java/com/dvops/maven/eclipse/HomeSevlet.java
@@ -6,6 +6,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.RequestDispatcher;
 
 /**
  * Servlet implementation class HomeSevlet
@@ -28,6 +29,8 @@ public class HomeSevlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/ReviewPage.jsp");
+		dispatcher.forward(request, response);
 	}
 
 	/**
